9ddcad44160ee432b4785add1515ec804b9c7c67
Nick Ray <nickrray@gmail.com>
2020-09-01 10:53:11 -0500

    :truck: Move server code from db directory to server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import cors from 'cors';
+import express from 'express';
+import { posts } from './routes/posts';
+import { users } from './routes/users';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_, res) => {
+  res.send('<h1>Hello from Express</h1>');
+});
+
+app.use('/posts', posts);
+app.use('/users', users);
+
+app.listen(PORT, () => {
+  console.log(`Listening at: http://localhost:${PORT}`);
+});
