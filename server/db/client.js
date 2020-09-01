8b9e5384a0f57b40c582415565c147384a6e2bdb
Nick Ray <nickrray@gmail.com>
2020-09-01 10:58:29 -0500

    :card_file_box: Create MongoClient and connection

---
+++ server/db/client.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+dotenv.config();
+
+const client = new MongoClient(
+  process.env.MONGODB_URI,
+  {
+    useUnifiedTopology: true,
+  },
+);
+
+(async () => {
+  await client.connect();
+
+  process.on('SIGINT', () => {
+    client.close().then(() => {
+      console.info('SIGINT received: DB connection is closing');
+      process.exit(0);
+    });
+  });
+})();
+
+export default client;
