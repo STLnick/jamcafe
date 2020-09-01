7909fe7ee8281aea57a3825241075e68aa3d0f40
Nick Ray <nickrray@gmail.com>
2020-09-01 11:52:32 -0500

    :hammer: Reconfigure webpack and package.json entry point/main file

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  target: 'node',
+  entry: './server.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
