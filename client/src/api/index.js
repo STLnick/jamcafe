9d5fa6202b08dd8ed29675bd70080f017bae6d63
Nick Ray <nickrray@gmail.com>
2020-09-01 17:25:35 -0500

    :construction: Fetch posts in database on initial render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,9 @@
-export default {
-  // TODO: Add api routes here
-};
+export default (baseUrl = 'http://localhost:5000') => ({
+  // Get posts and append the usernames to each
+  async getAllPosts() {
+    const postRes = await fetch(`${baseUrl}/posts`, {
+      contentType: 'application/json'
+    })
+    return await postRes.json()
+  }
+});
