0f702cab4514fa5ee9f739c109731e1f9ac33efd
Nick Ray <nickrray@gmail.com>
2020-09-02 12:16:31 -0500

    :construction: Combine properties of initial user object with edited field values
    Still need to make a PATCH request to update user in database

--- client/src/components/views/EditProfile/index.js
+++ client/src/components/views/EditProfile/index.js
@@ -10,7 +10,7 @@ export const EditProfile = () => {
     e.preventDefault()
 
     const editedUserInfo = utils.createObjectFromFields(e.target.elements)
-    const updatedUserInfo = { ...user, ...updatedUserInfo }
+    const updatedUserInfo = { ...user, ...editedUserInfo }
     console.log(updatedUserInfo)
 
     // TODO: Make PATCH request to server to update user
@@ -27,51 +27,53 @@ export const EditProfile = () => {
     // TODO: Redirect user to their own profile view (not editing) if successful
   }
 
-  return (<main class="edit-profile-container flex flex--column flex--align-center flex--justify-center">
-    <h3 class="section-heading">Edit Profile</h3>
+  return (<main className="edit-profile-container flex flex--column flex--align-center flex--justify-center">
+    <h3 className="section-heading">Edit Profile</h3>
     <form
-      class="edit-profile-form flex flex--column flex--align-center flex--justify-around"
-      onSubmit={() => handleSubmit()}
+      className="edit-profile-form flex flex--column flex--align-center flex--justify-around"
+      onSubmit={(e) => handleSubmit(e)}
     >
-      <label for="name" class="screen-reader-text">Name</label>
+      <label htmlFor="name" className="screen-reader-text">Name</label>
       <input
         id="name"
         type="text"
-        class="my-input"
+        className="my-input"
         placeholder="Name"
-        value={user.name}
+        defaultValue={user.name}
       />
-      <label for="bio" class="screen-reader-text">About Me</label>
-      <textarea id="bio" class="my-input" placeholder="About Me">
-        {user.about ? user.about : ''}
-      </textarea>
+      <label htmlFor="bio" className="screen-reader-text">About Me</label>
+      <textarea
+        id="bio"
+        className="my-input"
         defaultValue={user.bio ? user.bio : ''}
+        placeholder="About Me"></textarea>
+      <label htmlFor="location" className="screen-reader-text">Location</label>
       <input
         id="location"
         type="text"
-        class="my-input"
+        className="my-input"
         placeholder="Location (St Louis, MO)"
-        value={user.location ? user.location : ''}
+        defaultValue={user.location ? user.location : ''}
       />
-      <label for="genres" class="screen-reader-text">Genres</label>
+      <label htmlFor="genres" className="screen-reader-text">Genres</label>
       <input
         id="genres"
         type="text"
-        class="my-input"
+        className="my-input"
         placeholder="Genres you play (Rock, Blues, Metal)"
-        value={user.genres ? user.genres.join(', ') : ''}
+        defaultValue={user.genres ? user.genres.join(', ') : ''}
       />
-      <label for="instruments" class="screen-reader-text">Instruments</label>
+      <label htmlFor="instruments" className="screen-reader-text">Instruments</label>
       <input
         id="instruments"
         type="text"
-        class="my-input"
+        className="my-input"
         placeholder="Instruments (Guitar, Bass, Drums)"
-        value={user.instruments ? user.instruments.join(', ') : ''}
+        defaultValue={user.instruments ? user.instruments.join(', ') : ''}
       />
       {/* TODO: Implement cancel logic: clear fields? reset to original values? rediret to view profile? */}
-      <button class="cancel-btn">Cancel</button>
-      <button class="cta-btn" type="submit">Confirm</button>
+      <button className="cancel-btn">Cancel</button>
+      <button className="cta-btn" type="submit">Confirm</button>
     </form>
   </main>)
 }
