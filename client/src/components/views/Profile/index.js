eea669322b897a5a29889b297afc13c61d05a528
Nick Ray <nickrray@gmail.com>
2020-09-02 12:07:02 -0500

    :construction: Add button to Edit Profile in Profile View
    Need to only show the button if that user profile being viewed is the currently logged in user

--- client/src/components/views/Profile/index.js
+++ client/src/components/views/Profile/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 export const Profile = () => {
+  // TODO: Only show the edit profile button if this is the profile of
+  // TODO: the currently logged in user
+  const history = useHistory()
   const location = useLocation()
   const user = location.state.userProp
-  console.log(user)
-  // TODO: Remove hard code and receive a USER prop and then display accordingly
+
+  const handleEditProfileClick = () => {
+    // TODO: Change this to not just send in the user but to use the
+    // TODO: UserContext value that I should create using the
+    // TODO: usecontext() hook
+    history.push(`/profile/edit`, { userProp: user })
+  }
 
   return (<main className="view-profile-container flex flex--column flex--align-center flex--justify-center">
     <h3 className="section-heading">View Profile</h3>
+    {/* TEMPORARY BUTTON TO TEST EDIT PROFILE VIEW BY SENDING USER OBJ AS PROP */}
+    <button onClick={() => handleEditProfileClick()}>Edit Profile</button>
     <div className="profile-container flex flex--column flex--align-center flex--justify-around">
       <h4 id="name" className="profile-title">{user.username}</h4>
       <h6 className="profile-field-heading">Bio</h6>
