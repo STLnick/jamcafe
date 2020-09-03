65af0073cd93f426ffa392de83cdeaf1d951d467
Nick Ray <nickrray@gmail.com>
2020-09-03 16:37:55 -0500

    :construction: Render different mobile-menu links if user is logged in
    Need to add 'Make a Post' at the very least

--- client/src/components/base/Header/MobileMenu/index.js
+++ client/src/components/base/Header/MobileMenu/index.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-export const MobileMenu = ({ handleClick }) => {
+import { UserContext } from 'UserContext'
 
-  const links = [
-    {
-      path: '/',
-      text: 'Home'
-    },
-    {
-      path: '/register',
-      text: 'Sign Up'
-    },
-    {
-      path: '/login',
-      text: 'Login'
-    },
-    {
-      path: '/about',
-      text: 'About'
-    }
-  ]
+export const MobileMenu = ({ handleClick }) => {
+  const { user } = useContext(UserContext)
+
+  const links = user
+    ? [
+      {
+        path: `/profile/${user.username}`,
+        text: 'Profile'
+      },
+      {
+        path: '/about',
+        text: 'About'
+      }
+    ]
+    : [
+      {
+        path: '/',
+        text: 'Home'
+      },
+      {
+        path: '/register',
+        text: 'Sign Up'
+      },
+      {
+        path: '/login',
+        text: 'Login'
+      },
+      {
+        path: '/about',
+        text: 'About'
+      }
+    ]
 
   const renderLinks = () => links.map(({ path, text }, i) => (<li key={i}>
     <Link
