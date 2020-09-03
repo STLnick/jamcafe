e98c46bd47813a822c4d205e51d3c4e56f91b7cb
Nick Ray <nickrray@gmail.com>
2020-09-03 14:14:33 -0500

    :lock: Setup firebase client and connection

---
+++ client/src/auth/client.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase'
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyDnwsjBGL10glQx0OOqSbmnZP8n-vm-4hc",
+  authDomain: "jamcafe-capstone.firebaseapp.com",
+  databaseURL: "https://jamcafe-capstone.firebaseio.com",
+  projectId: "jamcafe-capstone",
+  storageBucket: "jamcafe-capstone.appspot.com",
+  messagingSenderId: "206675936450",
+  appId: "1:206675936450:web:41215d41e46a06b94be75b"
+})
