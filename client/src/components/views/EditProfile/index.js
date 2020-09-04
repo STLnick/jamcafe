5b3bb3dc5f0c2c237c0efb62d6f41e1b32138904
Nick Ray <nickrray@gmail.com>
2020-09-04 10:15:41 -0500

    :art: Use the user context to fetch info and display 'Loading' while fetching

--- client/src/components/views/EditProfile/index.js
+++ client/src/components/views/EditProfile/index.js
@@ -4,13 +4,19 @@ import utils from 'utils'
 
 export const EditProfile = () => {
   const location = useLocation()
-  const user = location.state.userProp
+  const [profileToEdit, setProfileToEdit] = useState(null)
+  useEffect(() => {
+    (async () => {
+      const profileRes = await repo.getUserByUsername(location.pathname.slice(14))
+      setProfileToEdit(profileRes)
+    })()
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     const editedUserInfo = utils.createObjectFromFields(e.target.elements)
-    const updatedUserInfo = { ...user, ...editedUserInfo }
+    const updatedUserInfo = { ...profileToEdit, ...editedUserInfo }
     console.log(updatedUserInfo)
 
     // TODO: Make PATCH request to server to update user
@@ -29,51 +35,53 @@ export const EditProfile = () => {
 
   return (<main className="edit-profile-container flex flex--column flex--align-center flex--justify-center">
     <h3 className="section-heading">Edit Profile</h3>
-    <form
-      className="edit-profile-form flex flex--column flex--align-center flex--justify-around"
-      onSubmit={(e) => handleSubmit(e)}
-    >
-      <label htmlFor="name" className="screen-reader-text">Name</label>
-      <input
-        id="name"
-        type="text"
-        className="my-input"
-        placeholder="Name"
-        defaultValue={user.name}
-      />
-      <label htmlFor="bio" className="screen-reader-text">About Me</label>
-      <textarea
-        id="bio"
-        className="my-input"
-        defaultValue={user.bio ? user.bio : ''}
-        placeholder="About Me"></textarea>
-      <label htmlFor="location" className="screen-reader-text">Location</label>
-      <input
-        id="location"
-        type="text"
-        className="my-input"
-        placeholder="Location (St Louis, MO)"
-        defaultValue={user.location ? user.location : ''}
-      />
-      <label htmlFor="genres" className="screen-reader-text">Genres</label>
-      <input
-        id="genres"
-        type="text"
-        className="my-input"
-        placeholder="Genres you play (Rock, Blues, Metal)"
-        defaultValue={user.genres ? user.genres.join(', ') : ''}
-      />
-      <label htmlFor="instruments" className="screen-reader-text">Instruments</label>
-      <input
-        id="instruments"
-        type="text"
-        className="my-input"
-        placeholder="Instruments (Guitar, Bass, Drums)"
-        defaultValue={user.instruments ? user.instruments.join(', ') : ''}
-      />
-      {/* TODO: Implement cancel logic: clear fields? reset to original values? rediret to view profile? */}
-      <button className="cancel-btn">Cancel</button>
-      <button className="cta-btn" type="submit">Confirm</button>
-    </form>
+    {profileToEdit
+      ? <form
+        className="edit-profile-form flex flex--column flex--align-center flex--justify-around"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <label htmlFor="name" className="screen-reader-text">Name</label>
+        <input
+          id="name"
+          type="text"
+          className="my-input"
+          placeholder="Name"
+          defaultValue={profileToEdit.name}
+        />
+        <label htmlFor="bio" className="screen-reader-text">About Me</label>
+        <textarea
+          id="bio"
+          className="my-input"
+          defaultValue={profileToEdit.bio ? profileToEdit.bio : ''}
+          placeholder="About Me"></textarea>
+        <label htmlFor="location" className="screen-reader-text">Location</label>
+        <input
+          id="location"
+          type="text"
+          className="my-input"
+          placeholder="Location (St Louis, MO)"
+          defaultValue={profileToEdit.location ? profileToEdit.location : ''}
+        />
+        <label htmlFor="genres" className="screen-reader-text">Genres</label>
+        <input
+          id="genres"
+          type="text"
+          className="my-input"
+          placeholder="Genres you play (Rock, Blues, Metal)"
+          defaultValue={profileToEdit.genres ? profileToEdit.genres.join(', ') : ''}
+        />
+        <label htmlFor="instruments" className="screen-reader-text">Instruments</label>
+        <input
+          id="instruments"
+          type="text"
+          className="my-input"
+          placeholder="Instruments (Guitar, Bass, Drums)"
+          defaultValue={profileToEdit.instruments ? profileToEdit.instruments.join(', ') : ''}
+        />
+        {/* TODO: Implement cancel logic: clear fields? reset to original values? redirect to view profile? */}
+        <button className="cancel-btn">Cancel</button>
+        <button className="cta-btn" type="submit">Confirm</button>
+      </form>
+      : <h3>Loading...</h3>}
   </main>)
 }
