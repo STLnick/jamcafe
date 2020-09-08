c02f154a07242d111c9759a098d83e9391f7bb1d
Nick Ray <nickrray@gmail.com>
2020-09-08 16:47:16 -0500

    :construction: Fetch all posts and users and store in state

--- client/src/components/views/Admin/index.js
+++ client/src/components/views/Admin/index.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import api from 'api'
+
+import './Admin.scss'
+
+const repo = api()
 
 export const Admin = () => {
-  return (<h1>Admin Page</h1>)
+  const [posts, setPosts] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      setPosts(await repo.getAllPosts())
+      setUsers(await repo.getAllUsers())
+    })()
+  }, [])
+
+  return (<div className="admin-container flex flex--column flex--align-center">
+    <h3 className="section-heading">Admin Dashboard</h3>
+    <table>
+      <caption>Users</caption>
+      <thead>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  </div>)
 }
