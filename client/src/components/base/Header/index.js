0463ff92a08788b7a1099203c9f7d8f64d3bd3e2
Nick Ray <nickrray@gmail.com>
2020-09-08 10:22:42 -0500

    :sparkles: Render SearchBar in Header when a user is logged in

--- client/src/components/base/Header/index.js
+++ client/src/components/base/Header/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import { MobileMenu } from './MobileMenu'
+import { SearchBar } from './SearchBar'
+import { UserContext } from 'UserContext'
 
 export const Header = ({ handleClick, handleKeyDown }) => {
+  const { user } = useContext(UserContext)
 
   return (
     <header className="top-nav flex flex--justify-between flex--align-center">
@@ -13,7 +16,7 @@ export const Header = ({ handleClick, handleKeyDown }) => {
           <img className="logo" src="img/logo.svg" alt="Jam Cafe logo" />
         </Link>
       </div>
-      {/* IF LOGGED IN - Display Search Bar */}
+      {user ? <SearchBar /> : null}
       <div className="top-nav-right flex flex--justify-around">
         <button
           className="menu-btn-container"
