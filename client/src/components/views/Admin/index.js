76ed7bc7104ee29257d6a58eca65e23148f7f4d9
Nick Ray <nickrray@gmail.com>
2020-09-08 17:20:10 -0500

    :sparkles: Display all users and posts with ability to switch between which is viewed

--- client/src/components/views/Admin/index.js
+++ client/src/components/views/Admin/index.js
@@ -7,8 +7,9 @@ import './Admin.scss'
 const repo = api()
 
 export const Admin = () => {
-  const [posts, setPosts] = useState([])
-  const [users, setUsers] = useState([])
+  const [posts, setPosts] = useState(null)
+  const [users, setUsers] = useState(null)
+  const [selectedView, setSelectedView] = useState('users')
 
   useEffect(() => {
     (async () => {
@@ -17,19 +18,38 @@ export const Admin = () => {
     })()
   }, [])
 
+  const handleChangeViewClick = () => setSelectedView(prevView => prevView === 'users' ? 'posts' : 'users')
+
+  const renderTableHeadings = (resource) => Object.keys(resource[0]).map(key => <td key={key}>{key}</td>)
+
+  const renderTableBodyRows = (resource) => {
+    const keys = Object.keys(resource[0])
+    return resource.map(el => <tr>
+      {keys.map(key => <td>{el[key]}</td>)}
+    </tr>)
+  }
+
   return (<div className="admin-container flex flex--column flex--align-center">
     <h3 className="section-heading">Admin Dashboard</h3>
-    <table>
-      <caption>Users</caption>
-      <thead>
-        <tr>
-          <td>Test 1</td>
-          <td>Test 2</td>
-        </tr>
-      </thead>
-      <tbody>
-
-      </tbody>
-    </table>
-  </div>)
+    <button
+      className="cta-btn"
+      onClick={handleChangeViewClick}
+    >
+      View {selectedView === 'users' ? 'Posts' : 'Users'}
+    </button>
+    {users
+      ? <table>
+        <caption className="post--title">{selectedView === 'users' ? 'Users' : 'Posts'}</caption>
+        <thead>
+          <tr>
+            {renderTableHeadings(selectedView === 'users' ? users : posts)}
+          </tr>
+        </thead>
+        <tbody>
+          {renderTableBodyRows(selectedView === 'users' ? users : posts)}
+        </tbody>
+      </table>
+      : <p className="is-size-4">Loading Data...</p>}
+  </div>
+  )
 }
