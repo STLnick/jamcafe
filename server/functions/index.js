bbccff93933a7a1d363cec278b5f4af0ed206814
Nick Ray <nickrray@gmail.com>
2020-09-10 16:37:49 -0500

    :construction: Deploy a Firebase cloud function to add an Admin Role to a user

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,4 +1,23 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 
 admin.initializeApp();
+
+// URL: https://us-central1-jamcafe-capstone.cloudfunctions.net/addAdminRole
+exports.addAdminRole = functions.https.onRequest(async (req, res) => {
+  cors(req, res, () => {
+    const { userEmail } = req.body;
+    admin.auth().getUserByEmail(userEmail)
+      .then((user) => admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      }))
+      .then(() => {
+        res.status(200).send(`Success! ${userEmail} has been made an admin.`);
+      })
+      .catch((err) => {
+        console.log('Error adding Admin claim: ', err);
+        res.status(500).send(err);
+      });
+  });
+});
