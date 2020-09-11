ff228ee521e1ae531db6a6297d2f05192f4de1ee
Nick Ray <nickrray@gmail.com>
2020-09-11 14:13:33 -0500

    :dizzy: Animate hero text

--- client/src/components/views/Home/Hero/index.js
+++ client/src/components/views/Home/Hero/index.js
@@ -9,8 +9,34 @@ const linkVariants = {
   visible: {
     opacity: 1,
     transition: {
-      delay: 1.0,
+      delay: 1.25,
       duration: 1.5
+    }
+  }
+}
+
+const textContainerVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      when: "beforeChildren",
+      staggerChildren: 0.3
+    }
+  }
+}
+
+const textVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5
     }
   }
 }
@@ -21,10 +47,37 @@ export const Hero = () => {
     <section
       className="section--hero flex flex--column flex--justify-end flex--align-center"
     >
-      <p className="hero-text">Find someone to jam with.</p>
-      <p className="hero-text">Find someone for your band.</p>
-      <p className="hero-text">Find a band to join.</p>
-      <p className="hero-text hero-text-last">Find people to play music with.</p>
+      <motion.div
+        className="hero-text-container"
+        initial="hidden"
+        animate="visible"
+        variants={textContainerVariants}
+      >
+        <motion.p
+          className="hero-text"
+          variants={textVariants}
+        >
+          Find someone to jam with.
+        </motion.p>
+        <motion.p
+          className="hero-text"
+          variants={textVariants}
+        >
+          Find someone for your band.
+        </motion.p>
+        <motion.p
+          className="hero-text"
+          variants={textVariants}
+        >
+          Find a band to join.
+        </motion.p>
+        <motion.p
+          className="hero-text hero-text-last"
+          variants={textVariants}
+        >
+          Find people to play music with.
+        </motion.p>
+      </motion.div>
       <motion.div
         initial="hidden"
         animate="visible"
