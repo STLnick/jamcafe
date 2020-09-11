afddbe4fb224303fccc06102c14ab4c5f650cdc9
Nick Ray <nickrray@gmail.com>
2020-09-11 17:00:23 -0500

    :lipstick: Style Modal and content for filtering in Feed

--- client/src/components/base/Header/SearchBar/index.js
+++ client/src/components/base/Header/SearchBar/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Modal from 'react-modal'
 import PropTypes from 'prop-types'
 
+import './SearchBar.scss'
 import { ReactComponent as FunnelIcon } from '../../../../assets/funnel.svg'
 
 export const SearchBar = ({ handleSearchSelectionChange, handleSearchTextChange, searchSelection, searchText }) => {
@@ -22,53 +23,52 @@ export const SearchBar = ({ handleSearchSelectionChange, handleSearchTextChange,
     >
       <FunnelIcon className="filter-primary search-icon" />
     </button>
-    <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={() => setModalIsOpen(false)}
+      style={{
+        content: {
+          height: '275px',
+          left: '50%',
+          top: '25%',
+          transform: 'translate(-50%, -50%)',
+          width: '200px',
+        }
+      }}
+    >
       {/* Checkboxes for filter selection */}
-      <form onSubmit={(e) => {
-        handleSearchSelectionChange(e)
-        setModalIsOpen(false)
-      }}>
-        <label htmlFor="filter-choice"></label>
-        <select
-          name="filter-choice"
-          onChange={(e) => setSelectValue(e.target.value)}
-          id="filter-choice"
-          value={selectValue}>
-          <option value="title">
-            Title
-          </option>
-          <option value="username">
-            Username
-          </option>
-        </select>
-
-        {/* <label className="checkbox-container flex flex--align-center is-size-4" htmlFor="title">
-          Title
-        <input
-            defaultChecked={searchSelection === 'title'}
-            id="title"
-            name="title"
-            type="checkbox"
-            value="title"
-          />
-          <span className="checkmark"></span>
-        </label>
-        <label className="checkbox-container flex flex--align-center is-size-4" htmlFor="username">
-          Username
-        <input
-            defaultChecked={searchSelection === 'username'}
-            id="username"
-            name="username"
-            type="checkbox"
-            value="username"
-          />
-          <span className="checkmark"></span>
-        </label> */}
-        <button className="cancel-btn" onClick={() => setModalIsOpen(false)}>Cancel</button>
-        <button className="cta-btn" type="submit">Confirm</button>
+      <form
+        className="filter-modal-form flex flex--column flex--align-center flex--justify-between"
+        onSubmit={(e) => {
+          handleSearchSelectionChange(e)
+          setModalIsOpen(false)
+        }}
+      >
+        <label className="medium-label" htmlFor="filter-choice">Filter By</label>
+        <div
+          className="select is-medium is-primary flex flex--column flex--align-center flex--justify-center"
+        >
+          <select
+            name="filter-choice"
+            onChange={(e) => setSelectValue(e.target.value)}
+            id="filter-choice"
+            value={selectValue}
+          >
+            <option value="title">
+              Title
+            </option>
+            <option value="username">
+              Username
+            </option>
+          </select>
+        </div>
+        <div className="flex flex--column flex--align-center flex--justify-center">
+          <button className="cancel-btn" onClick={() => setModalIsOpen(false)}>Cancel</button>
+          <button className="cta-btn" type="submit">Confirm</button>
+        </div>
       </form>
     </Modal>
-  </div>)
+  </div >)
 }
 
 SearchBar.propTypes = {
