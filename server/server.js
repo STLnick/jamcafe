a2335b012d077084e02823fa96ba95f2327e9047
Nick Ray <nickrray@gmail.com>
2020-09-14 16:43:47 -0500

    :construction: Bring in http module for server to integrate socket.io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import cors from 'cors';
 import express from 'express';
+import http from 'http';
+
 import chats from './routes/chats';
 import posts from './routes/posts';
 import users from './routes/users';
 
 const app = express();
+const server = http.createServer(app);
 const PORT = 5000;
 
 app.use(cors());
@@ -18,6 +21,6 @@ app.use('/chats', chats);
 app.use('/posts', posts);
 app.use('/users', users);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening at: http://localhost:${PORT}`);
 });
